7fd2be7d84e216c35d11c827e526aabacc7f8630
Jhonata Costa <costajhonata8@gmail.com>
2024-05-04 23:06:32 -0300

    Post for champion part-2

--- src/main/java/com/example/clientStore/repository/ChampionRepository.java
+++ src/main/java/com/example/clientStore/repository/ChampionRepository.java
@@ -6,4 +6,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChampionRepository extends JpaRepository<Champion, Long> {
+
+    Champion findByName(String name);
+
+    Champion findByTitle(String title);
+
+    Champion findByImageUrl(String title);
 }
--- src/main/java/com/example/clientStore/service/ChampionService.java
+++ src/main/java/com/example/clientStore/service/ChampionService.java
@@ -3,9 +3,12 @@ package com.example.clientStore.service;
 import com.example.clientStore.model.Champion;
 import com.example.clientStore.model.dto.input.ChampionInputDTO;
 import com.example.clientStore.repository.ChampionRepository;
+import com.example.clientStore.service.exception.BadRequestException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 import static com.example.clientStore.model.enums.ChampionRoleEnum.getChampionRoleEnum;
 
@@ -20,10 +23,36 @@ public class ChampionService {
 
     public Champion saveChampion(ChampionInputDTO championInputDTO) {
         Champion champion = modelMapper.map(championInputDTO, Champion.class);
+
+        //TODO: One champion can have more than one role
         champion.setRole(getChampionRoleEnum(championInputDTO.getRole()));
 
-        //TODO: exception for already existing name, title and imgUrl
+        findChampionByName(champion.getId(), champion.getName());
+        findChampionByTitle(champion.getId(), champion.getTitle());
+        findChampionByImageUrl(champion.getId(), champion.getImageUrl());
+
         champion = championRepository.save(champion);
         return champion;
+    }
+
+    private void findChampionByName(Long id, String name){
+        Champion champion = championRepository.findByName(name);
+        if(Objects.nonNull(champion) && !Objects.equals(id, champion.getId())){
+            throw new BadRequestException("This Champion name is already in use!");
+        }
+    }
+
+    private void findChampionByTitle(Long id, String title){
+        Champion champion = championRepository.findByTitle(title);
+        if(Objects.nonNull(champion) && !Objects.equals(id, champion.getId())){
+            throw new BadRequestException("This Champion title is already in use!");
+        }
+    }
+
+    private void findChampionByImageUrl(Long id, String title){
+        Champion champion = championRepository.findByImageUrl(title);
+        if(Objects.nonNull(champion) && !Objects.equals(id, champion.getId())){
+            throw new BadRequestException("This Champion image is already in use!");
+        }
     }
 }
